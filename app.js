fe1348156c1cd78afc2a101c5fc89f283ff57003
Gianfranco Fertino Hwandiano <gianffh@gmail.com>
2017-06-23 13:25:32 +0000

    + UI & database

--- app.js
+++ app.js
@@ -1,17 +1,68 @@
-var express = require("express");
-var app = express();
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var Campground = mongoose.model(
+                 "Campground", 
+                 new mongoose.Schema({
+                    name: String,
+                    image: String, 
+                    desc: String
+                 })
+                 );
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+//         desc: "It's beautiful !!!!!! Trust me you won't regret this experience."
+//     },
+//     function(err, created) {
+//         if (err)
+//             console.log(err);
+//         else
+//             console.log(created);
+//     }
+// );
 
 app.get("/", function(req, res){
     res.render("landing.ejs");
 })
 
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+})
+
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, found) {
+        if (err)
+            console.log(err);
+        else
+            res.render("show.ejs", {Camp: found});
+    });
+})
+
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Granite Hill", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-        {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2535/3823437635_c712decf64.jpg"},
-        {name: "Mountain Goat", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"}
-    ];
-    res.render("campgrounds.ejs", {campgrounds: campgrounds});
+    Campground.find({}, function(err, campgrounds){
+        if (err)
+            console.log(err);
+        else
+            res.render("index.ejs", {campgrounds: campgrounds});
+    })
+})
+
+app.post("/campgrounds", function(req, res){
+    Campground.create(req.body, function(err, created){
+        if (err)
+            console.log(err);
+        else
+            console.log("Succesfully added " + created);
+    });
+    res.redirect("/campgrounds");
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
