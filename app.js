7a4011499e16cd78fe2ba3d3d365c57239393bf1
Gianfranco Fertino Hwandiano <gianffh@gmail.com>
2017-07-15 17:34:02 +0000

    comment styled

--- app.js
+++ app.js
@@ -3,49 +3,86 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./model/campground.js"),
+    Comment     = require("./model/comment.js"),
     seedDB      = require("./seeds.js");
 
+//setting up database
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
+//-----CAMPGROUND ROUTE--------------
+
+//Main Page
 app.get("/", function(req, res){
     res.render("landing.ejs");
-})
+});
 
+//New Campground
 app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs");
-})
+    res.render("campground/new.ejs");
+});
 
+//Showing a particular campground
 app.get("/campgrounds/:id", function(req, res){
     Campground.findById(req.params.id).populate("comments").exec(function(err, found) {
         if (err)
             console.log("err");
         else
-            console.log(found);
-            res.render("show.ejs", {Camp: found});
+            res.render("campground/show.ejs", {Camp: found});
     });
-})
+});
 
+//Index Page, showing list of campgrounds
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, campgrounds){
         if (err)
             console.log(err);
         else
-            res.render("index.ejs", {campgrounds: campgrounds});
+            res.render("campground/index.ejs", {campgrounds: campgrounds});
     })
-})
+});
 
+//Post Route, receiving new campground and redirecting to /campgrounds
 app.post("/campgrounds", function(req, res){
-    Campground.create(req.body, function(err, created){
+    Campground.create(req.body.campground, function(err, created){
         if (err)
             console.log(err);
         else
             console.log("Succesfully added " + created);
     });
     res.redirect("/campgrounds");
-})
+});
+
+//--------COMMENT ROUTE---------
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if (err)
+            console.log(err);
+        else
+            res.render("comment/new.ejs", {campground: campground});
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err)
+            console.log(err);
+        else {
+            Comment.create(req.body.comment, function(err, createdComment){
+                if (err)
+                    console.log(err);
+                else {
+                    foundCampground.comments.push(createdComment);
+                    foundCampground.save();
+                    res.redirect("/campgrounds/" + req.params.id);
+                }
+            });
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YELP CAMP HAS STARTED!");
-})
+});
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Granite Hill", 
         image: "https://farm5.staticflickr.com/4027/4368764673_c8345bd602.jpg", 
-        desc: "It's beautiful !!!!!! Trust me you won't regret this experience."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Somewhere Cool", 
         image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg", 
-        desc: "It's beautiful !!!!!! Trust me you won't regret this experience."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Beach", 
         image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg", 
-        desc: "It's beautiful !!!!!! Trust me you won't regret this experience."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 ];    
 
